389392205500edad72913c4f03d7b511dc334d23
fdev31 <fdev31@gmail.com>
2023-08-01 18:03:56 +0200

    back to catppuccin

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-    theme = "bearded-arc",
-    theme_toggle = { "everblush", "bearded-arc" },
+    theme = "catppuccin",
+    theme_toggle = { "catppuccin", "bearded-arc" },
     -- transparency = true,
 
     hl_override = highlights.override,
