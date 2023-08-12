e9eb4bae73085abe93547494385a0dcdb15c2764
fdev31 <fdev31@gmail.com>
2023-08-12 14:52:32 +0200

    add volar & tailwind (vue3)

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,11 +3,25 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
-local servers = { "pylsp", "html", "cssls", "tsserver", "clangd", "eslint" }
+local servers = { "pylsp", "html", "cssls", "clangd", "tailwindcss", "tsserver", "volar" }
+
 
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
+    if not lsp == "volar" then
+        lspconfig[lsp].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+        })
+    end
 end
+
+lspconfig.volar.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {'vue', 'json'},
+	init_options = {
+    typescript = {
+            tsdk = "/usr/lib/node_modules/typescript/lib/"
+		},
+	},
+})
