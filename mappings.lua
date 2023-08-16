e5921b21da0dac348d4985418ee982fe22bc176e
fdev31 <fdev31@gmail.com>
2023-08-16 10:07:03 +0200

    fix mappings

--- mappings.lua
+++ mappings.lua
@@ -1,21 +1,120 @@
 local lib = require("custom.lib")
 
 ---@type MappingsTable
-local M = {}
 
 require("core.mappings").nvterm = nil
 
-M.disabled = {
-	v = {
-		["<leader>/"] = "",
-		["<leader>ra"] = "",
-	},
-	n = {
-		["<leader>/"] = "",
-		["<leader>ra"] = "",
-	},
+local M = {
+	disabled = { -- {{{
+		v = {
+			["<leader>/"] = "",
+			["<leader>ra"] = "",
+		},
+		n = {
+			["<leader>/"] = "",
+			["<leader>ra"] = "",
+		},
+	}, -- }}}
+	general = { -- {{{
+		n = {
+			["<leader>n"] = { "<cmd> set rnu! <CR>", "Toggle relative number", { noremap = true, silent = true } },
+		},
+		v = {
+			["<A-down>"] = { ":MoveBlock(1)<CR>", "Move down", { noremap = true, silent = true } },
+			["<A-up>"] = { ":MoveBlock(-1)<CR>", "Move up", { noremap = true, silent = true } },
+			["<A-left>"] = { ":MoveHBlock(-1)<CR>", "Move left", { noremap = true, silent = true } },
+			["<A-right>"] = { ":MoveHBlock(1)<CR>", "Move right", { noremap = true, silent = true } },
+		},
+	}, -- }}}
+	comment = { -- {{{
+		plugin = true,
+
+		-- toggle comment in both modes
+		n = {
+			["<leader>c"] = {
+				function()
+					require("Comment.api").toggle.linewise.current()
+				end,
+				"Toggle comment",
+			},
+		},
+
+		v = {
+			["<leader>c"] = {
+				"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+				"Toggle comment",
+			},
+		},
+	}, -- }}}
+	gitsigns = { -- {{{
+		n = {
+			["<leader>sh"] = {
+				function()
+					package.loaded.gitsigns.stage_hunk()
+				end,
+				"Stage hunk",
+			},
+			["<leader>uh"] = {
+				function()
+					package.loaded.gitsigns.undo_stage_hunk()
+				end,
+				"Revert saged hunk",
+			},
+			["<leader>D"] = {
+				function()
+					package.loaded.gitsigns.diffthis()
+				end,
+				"Show diff",
+			},
+		},
+	}, -- }}}
+	lspconfig = { -- {{{
+		n = {
+			["<leader>rn"] = {
+				function()
+					require("nvchad.renamer").open()
+				end,
+				"LSP rename",
+			},
+		},
+	}, -- }}}
+	telescope = { -- {{{
+		n = {
+			["<C-,>"] = {
+				function()
+					require("telescope").extensions.aerial.aerial()
+				end,
+				"Toggle code outline window",
+			},
+		},
+	}, -- }}}
 }
 
+-- Conditional:
+
+-- vim diff {{{
+if vim.opt.diff:get() then
+	M.diffmode = {
+		plugin = false,
+		n = {
+			["dr"] = {
+				"<cmd>diffget REMOTE<CR>",
+				"get remote",
+			},
+			["dl"] = {
+				"<cmd>diffget LOCAL<CR>",
+				"get local",
+			},
+			["db"] = {
+				"<cmd>diffget BASE<CR>",
+				"get base",
+			},
+		},
+	}
+end
+-- }}}
+
+-- codeium {{{
 if not lib.isWorkLaptop then
 	M.codeium = {
 		i = {
@@ -52,102 +151,6 @@ if not lib.isWorkLaptop then
 		},
 	}
 end
-
-M.general = {
-	n = {
-		["<leader>n"] = { "<cmd> set rnu! <CR>", "Toggle relative number" },
-	},
-	v = {
-		["<A-down>"] = { ":MoveBlock(1)<CR>", "Move down", { noremap = true, silent = true } },
-		["<A-up>"] = { ":MoveBlock(-1)<CR>", "Move up", { noremap = true, silent = true } },
-		["<A-left>"] = { ":MoveHBlock(-1)<CR>", "Move left", { noremap = true, silent = true } },
-		["<A-right>"] = { ":MoveHBlock(1)<CR>", "Move right", { noremap = true, silent = true } },
-	},
-}
-
-M.comment = {
-	plugin = true,
-
-	-- toggle comment in both modes
-	n = {
-		["<leader>c"] = {
-			function()
-				require("Comment.api").toggle.linewise.current()
-			end,
-			"Toggle comment",
-		},
-	},
-
-	v = {
-		["<leader>c"] = {
-			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-			"Toggle comment",
-		},
-	},
-}
--- for vim diff / mergetool {{{
-M.general = {
-	n = {
-		["dr"] = {
-			"<cmd>diffget REMOTE<CR>",
-			"get remote",
-		},
-		["dl"] = {
-			"<cmd>diffget LOCAL<CR>",
-			"get local",
-		},
-		["db"] = {
-			"<cmd>diffget BASE<CR>",
-			"get base",
-		},
-	},
-}
 -- }}}
-
-M.gitsigns = {
-	n = {
-		["<leader>sh"] = {
-			function()
-				package.loaded.gitsigns.stage_hunk()
-			end,
-			"Stage hunk",
-		},
-		["<leader>uh"] = {
-			function()
-				package.loaded.gitsigns.undo_stage_hunk()
-			end,
-			"Revert saged hunk",
-		},
-		["<leader>D"] = {
-			function()
-				package.loaded.gitsigns.diffthis()
-			end,
-			"Show diff",
-		},
-	},
-}
-
-M.lspconfig = {
-	n = {
-		["<leader>rn"] = {
-			function()
-				require("nvchad.renamer").open()
-			end,
-			"LSP rename",
-		},
-	},
-}
-M.telescope = {
-	n = {
-		["<C-,>"] = {
-			function()
-				require("telescope").extensions.aerial.aerial()
-			end,
-			"Toggle code outline window",
-		},
-	},
-}
-
--- more keybinds!
 
 return M
