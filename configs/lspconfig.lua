48d63cbea9cd0a90387ff1e003bfec493bd0b701
fdev31 <fdev31@gmail.com>
2023-11-09 18:17:50 +0100

    rework pylsp config

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -6,13 +6,15 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 
 local servers = { "pylsp", "html", "cssls", "clangd", "tsserver", "volar", "eslint", "lua_ls" }
+local custom_setup = { "volar", "pylsp" } -- list of manually configured servers
+custom_setup.has = lib.has
 
 if not lib.isWorkLaptop then
-    table.insert(servers, "tailwindcss")
+	table.insert(servers, "tailwindcss")
 end
 
 for _, lsp in ipairs(servers) do
-	if lsp ~= "volar" then
+	if not custom_setup:has(lsp) then
 		lspconfig[lsp].setup({
 			on_attach = on_attach,
 			capabilities = capabilities,
@@ -27,6 +29,30 @@ lspconfig.volar.setup({
 	init_options = {
 		typescript = {
 			tsdk = "/usr/lib/node_modules/typescript/lib/",
+		},
+	},
+})
+
+lspconfig.pylsp.setup({
+	on_attach = on_attach,
+	settings = {
+		pylsp = {
+			plugins = {
+				-- formatter options
+				black = { enabled = true },
+				autopep8 = { enabled = true },
+				yapf = { enabled = false },
+				-- linter options
+				pylint = { enabled = false },
+				pyflakes = { enabled = true },
+				pycodestyle = { enabled = true },
+				-- type checker
+				pylsp_mypy = { enabled = true },
+				-- auto-completion options
+				jedi_completion = { fuzzy = true },
+				-- import sorting
+				pyls_isort = { enabled = true },
+			},
 		},
 	},
 })
--- lib.lua
+++ lib.lua
@@ -10,6 +10,15 @@ local function file_exists(name)
 	end
 end
 
+exported.has = function(tbl, item)
+	for _, v in ipairs(tbl) do
+		if v == item then
+			return true
+		end
+	end
+	return false
+end
+
 exported.isWorkLaptop = file_exists("/home/fab/liberty/code")
 
 exported.isGitMergetool = vim.env.TEXTDOMAIN == "git" or vim.env.GIT_PREFIX ~= nil
